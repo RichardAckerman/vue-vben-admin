293b595b2a53b314af4e840f5315fc7f8b755830
davoschen <davoschen@xiaoman.com>
2023-10-19 10:59:05 +0800

    test: 测试rxjs

--- src/views/sys/test/index.vue
+++ src/views/sys/test/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>asfafsd</div>
-  <Button type="primary">按钮</Button>
+  <div>
+    asfafsd
+    <Button type="primary">按钮</Button>
+  </div>
 </template>
 <script lang="ts" setup>
   import { Button } from 'ant-design-vue';
+  import { Observable } from 'rxjs';
+
+  // const promise = new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve('Hello from a Promise!');
+  //   }, 2000);
+  // });
+
+  // promise.then((value) => console.log(value));
+
+  const observable = new Observable((observer) => {
+    setTimeout(() => {
+      observer.next('Hello from a Observable!');
+    }, 2000);
+  });
+
+  observable.subscribe((value) => console.log(value));
 </script>
